9fdbdfa22baf1149b1348c7326af88ec852c32c7
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-06-11 16:59:14 +0300

    Adding Problem 05. Online Store Database from Data Relations

--- E05.Table Relations/E05.Table Relations/SQLQuery1.sql
+++ E05.Table Relations/E05.Table Relations/SQLQuery1.sql
@@ -117,5 +117,28 @@ VALUES
 
 -- P04. Self-Referencing 
 
+CREATE TABLE Teachers (
+	TeacherID INT NOT NULL IDENTITY (101 , 1),
+	Name NVARCHAR(50) NOT NULL,
+	ManagerID INT
+
+	CONSTRAINT PK_TeacherID PRIMARY KEY (TeacherID),
+	CONSTRAINT FK_Teachers_Teachers FOREIGN KEY (ManagerID) REFERENCES Teachers (TeacherID)
+	)
+
+INSERT INTO Teachers
+VALUES 
+('John', NULL),
+('Maya', 106),
+('Silvia', 106),
+('Ted', 105),
+('Mark', 101),
+('Greta', 101)
+
+-- P05. Online Store Database
+
+
+
+
 
 
