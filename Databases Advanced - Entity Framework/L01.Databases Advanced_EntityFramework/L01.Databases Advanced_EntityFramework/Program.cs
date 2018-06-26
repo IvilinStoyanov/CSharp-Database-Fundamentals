792a8880e2fa6187112e388783b3198ddaa7f47a
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-06-26 17:45:47 +0300

    Adding the DATEBASE Minions DB

---
+++ Databases Advanced - Entity Framework/L01.Databases Advanced_EntityFramework/L01.Databases Advanced_EntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Databases_Advanced_EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
