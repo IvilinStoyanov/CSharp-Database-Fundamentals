bf86e80f7c0882c2621dec7d0c4fdb9512c87a31
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-06-27 13:27:07 +0300

    Connecting to the DB

---
+++ Databases Advanced - Entity Framework/L01.Databases Advanced_EntityFramework/L01.Databases Advanced_EntityFramework/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Databases_Advanced_EntityFramework
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.Open();
+
+                string datebaseSql = ""
+
+                connection.Close();
+            }
+        }
+    }
+}
