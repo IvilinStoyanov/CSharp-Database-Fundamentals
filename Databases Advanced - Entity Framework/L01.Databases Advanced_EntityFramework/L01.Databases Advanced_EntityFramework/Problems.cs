a43f2e16b5674807007d021d6f583d4b3cb70da5
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-06-28 17:27:14 +0300

    Adding till P09

--- Databases Advanced - Entity Framework/L01.Databases Advanced_EntityFramework/L01.Databases Advanced_EntityFramework/Problems.cs
+++ Databases Advanced - Entity Framework/L01.Databases Advanced_EntityFramework/L01.Databases Advanced_EntityFramework/Problems.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace Fetching_Resultsets_ADO.NET
 {
     public class Problems
     {
+        // P01
         public void InitialSetup(string connectionString)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -75,6 +77,7 @@ namespace Fetching_Resultsets_ADO.NET
             }
         }
 
+        // P02
         public void VillainNames(string connectionString)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -87,10 +90,10 @@ namespace Fetching_Resultsets_ADO.NET
                 {
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
-                      while(reader.Read())
+                        while (reader.Read())
                         {
                             Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
-                        }               
+                        }
                     }
                 }
 
@@ -98,6 +101,7 @@ namespace Fetching_Resultsets_ADO.NET
             }
         }
 
+        // P03
         public void MinionNames(string connectionString)
         {
             var villainId = int.Parse(Console.ReadLine());
@@ -106,9 +110,9 @@ namespace Fetching_Resultsets_ADO.NET
             {
                 connection.Open();
 
-                string villianName = GetVillianName(villainId, connection);
+                string villianName = GetVillainNames(villainId, connection);
 
-                if(villianName == null)
+                if (villianName == null)
                 {
                     Console.WriteLine($"No villain with ID {villainId} exists in the database.");
                 }
@@ -127,7 +131,7 @@ namespace Fetching_Resultsets_ADO.NET
                 //    {
                 //        reader.Read();
                 //       Console.WriteLine($"Villian: {reader["Name"]}");
-                        
+
                 //        while (reader.Read())
                 //        {
                 //            int count = 0;
@@ -137,6 +141,344 @@ namespace Fetching_Resultsets_ADO.NET
                 //}
 
                 connection.Close();
+            }
+        }
+
+        // P04
+        public void AddMinion(string connectionString)
+        {
+            string[] minionsInfo = Console.ReadLine().Split();
+            string[] villainInfo = Console.ReadLine().Split();
+
+            string minionName = minionsInfo[1];
+            int age = int.Parse(minionsInfo[2]);
+            string townName = minionsInfo[3];
+
+            string villainName = villainInfo[1];
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                int townId = GetTownId(townName, connection);
+                int villainId = GetvillainId(villainName, connection);
+                int minionId = InsertMinionAndGetId(minionName, age, townId, connection);
+
+                AssingMinionToVillain(villainId, minionId, connection);
+                Console.WriteLine($"Successfully added {minionName} to be minion of {villainName}.");
+                connection.Close();
+            }
+        }
+
+        // P05
+        public void ChangeTownNamesCasing(string conneciotnString)
+        {
+            string countryName = Console.ReadLine();
+
+            using (SqlConnection connection = new SqlConnection(conneciotnString))
+            {
+                connection.Open();
+
+                int countryId = GetCountryID(countryName, connection);
+
+                if (countryId == 0)
+                {
+                    Console.WriteLine("No town names were affected.");
+                }
+                else
+                {
+                    int affectedRows = UpdateNames(countryId, connection);
+                    List<string> names = GetNames(countryId, connection);
+                    Console.WriteLine($"{affectedRows} town names were affected");
+                    Console.WriteLine($"[{string.Join(", ", names)}]");
+                }
+
+                connection.Close();
+            }
+        }
+
+        //P07
+        public void PrintAllMinionsNames(string connectionString)
+        {
+            List<string> minionsInitial = new List<string>();
+            List<string> minionsArranged = new List<string>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string allMinionNamesSql = "SELECT Name FROM Minions";
+
+                using (SqlCommand command = new SqlCommand(allMinionNamesSql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            minionsInitial.Add((string)reader["Name"]);
+                        }
+                    }
+
+                    while (minionsInitial.Count > 0)
+                    {
+                        minionsArranged.Add(minionsInitial[0]);
+                        minionsInitial.RemoveAt(0);
+
+                        if (minionsInitial.Count > 0)
+                        {
+                            minionsArranged.Add(minionsInitial[minionsInitial.Count - 1]);
+                            minionsInitial.RemoveAt(minionsInitial.Count - 1);
+                        }
+                    }
+
+                    minionsArranged.ForEach(m => Console.WriteLine(m));
+                }
+                connection.Close();
+            }
+        }
+
+        //P08 
+        public void IncreaseMinionAge(string connectionString)
+        {
+            int[] selectedIds = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SqlConnection dbCon = new SqlConnection(connectionString);
+            dbCon.Open();
+
+            List<int> minionIds = new List<int>();
+            List<string> minionaNames = new List<string>();
+            List<int> minionAges = new List<int>();
+
+            using (dbCon)
+            {
+                SqlCommand command = new SqlCommand($"SELECT * FROM Minions WHERE Id IN ({String.Join(", ", selectedIds)})", dbCon);
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    if (!reader.HasRows)
+                    {
+                        reader.Close();
+                        dbCon.Close();
+                        return;
+                    }
+
+                    while (reader.Read())
+                    {
+                        minionIds.Add((int)reader["Id"]);
+                        minionaNames.Add((string)reader["Name"]);
+                        minionAges.Add((int)reader["Age"]);
+                    }
+                }
+
+                for (int i = 0; i < minionIds.Count; i++)
+                {
+                    int id = minionIds[i];
+                    string name = String.Join(" ", minionaNames[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList().Select(n => n = char.ToUpper(n.First()) + n.Substring(1).ToLower()).ToArray());
+                    int age = minionAges[i] + 1;
+
+                    command = new SqlCommand("UPDATE Minions SET Name = @name, Age = @age WHERE Id = @Id", dbCon);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@age", age);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    command.ExecuteNonQuery();
+                }
+
+                command = new SqlCommand($"SELECT * FROM Minions", dbCon);
+                reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    if (!reader.HasRows)
+                    {
+                        reader.Close();
+                        dbCon.Close();
+                        return;
+                    }
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{(int)reader["Id"]} {(string)reader["Name"]} {(int)reader["Age"]}");
+                    }
+                }
+            }
+        }
+
+        //P09 
+        public void IncreaseAgeStoredProcedure(string connectionString)
+        {
+            SqlConnection dbCon = new SqlConnection(connectionString);
+            dbCon.Open();
+
+            int id = int.Parse(Console.ReadLine());
+
+            using (dbCon)
+            {
+                var command = new SqlCommand("EXEC usp_GetOlder @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", id);
+
+                command.ExecuteNonQuery();
+
+                command = new SqlCommand("SELECT * FROM Minions WHERE Id = @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", id);
+
+                var reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    reader.Read();
+
+                    Console.WriteLine($"{(string)reader["Name"]} - {(int)reader["Age"]} years old");
+                }
+            }
+        }
+
+        private List<string> GetNames(int countryId, SqlConnection connection)
+        {
+            List<string> names = new List<string>();
+
+            string namesSql = "SELECT Name FROM Towns WHERE CountryCode = @countryId";
+
+            using (SqlCommand command = new SqlCommand(namesSql, connection))
+            {
+                command.Parameters.AddWithValue("@countryId", countryId);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        names.Add((string)reader[0]);
+                    }
+                }
+            }
+            return names;
+        }
+
+        private static int UpdateNames(int countryId, SqlConnection connection)
+        {
+            string updateNames = "UPDATE Towns SET Name = UPPER(Name) WHERE CountryCode = @CountryId";
+
+            using (SqlCommand command = new SqlCommand(updateNames, connection))
+            {
+                command.Parameters.AddWithValue("@CountryId", countryId);
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        private int GetCountryID(string countryName, SqlConnection connection)
+        {
+            string countryInfo = "SELECT TOP(1) c.Id FROM Towns AS t JOIN Countries AS c ON c.Id = t.CountryCode WHERE c.Name = @name";
+
+            using (SqlCommand command = new SqlCommand(countryInfo, connection))
+            {
+                command.Parameters.AddWithValue("@name", countryName);
+                if (command.ExecuteScalar() == null)
+                {
+                    return 0;
+                }
+                return (int)command.ExecuteScalar();
+            }
+        }
+
+        private void AssingMinionToVillain(int villainId, int minionId, SqlConnection connection)
+        {
+            string insertMinionToVillain = "INSERT INTO MinionsVillains(MinionId, VillainId) VALUES (@minionId, @villainId)";
+
+            using (SqlCommand command = new SqlCommand(insertMinionToVillain, connection))
+            {
+                command.Parameters.AddWithValue("@minionId", minionId);
+                command.Parameters.AddWithValue("@villainId", villainId);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private int InsertMinionAndGetId(string minionName, int age, int townId, SqlConnection connection)
+        {
+            string insertMinion = "INSERT INTO Minions (Name, Age, TownId) VALUES (@name, @age, @townId)";
+
+            using (SqlCommand command = new SqlCommand(insertMinion, connection))
+            {
+                command.Parameters.AddWithValue("@name", minionName);
+                command.Parameters.AddWithValue("@age", age);
+                command.Parameters.AddWithValue("@townId", townId);
+                command.ExecuteNonQuery();
+            }
+
+            string minionsSql = "SELECT Id FROM Minions WHERE Name = @name";
+
+            using (SqlCommand command = new SqlCommand(minionsSql, connection))
+            {
+                command.Parameters.AddWithValue("@name", minionName);
+                return (int)command.ExecuteScalar();
+            }
+        }
+
+        private int GetvillainId(string villainName, SqlConnection connection)
+        {
+            string townSql = "SELECT Id FROM Villains WHERE Name = @Name";
+
+            using (SqlCommand command = new SqlCommand(townSql, connection))
+            {
+                command.Parameters.AddWithValue("@Name", villainName);
+
+                if (command.ExecuteScalar() == null)
+                {
+                    InsertIntoVillian(villainName, connection);
+                    Console.WriteLine($"Villain {villainName} was added to the database");
+                }
+                return (int)command.ExecuteScalar();
+            }
+        }
+
+        private void InsertIntoVillian(string villainName, SqlConnection connection)
+        {
+            string insertTowns = "INSERT INTO Villains(Name) VALUES (@villianName)";
+
+            using (SqlCommand command = new SqlCommand(insertTowns, connection))
+            {
+                command.Parameters.AddWithValue("@villianName", villainName);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private int GetTownId(string townName, SqlConnection connection)
+        {
+            string townSql = "SELECT Id FROM Towns WHERE Name = @Name";
+
+            using (SqlCommand command = new SqlCommand(townSql, connection))
+            {
+                command.Parameters.AddWithValue("@Name", townName);
+
+                if (command.ExecuteScalar() == null)
+                {
+                    InsertIntoTowns(townName, connection);
+                    Console.WriteLine($"Town {townName} was added to the database");
+                }
+                return (int)command.ExecuteScalar();
+            }
+        }
+
+        private void InsertIntoTowns(string townName, SqlConnection connection)
+        {
+            string insertTowns = "INSERT INTO Towns(Name) VALUES (@townName)";
+
+            using (SqlCommand command = new SqlCommand(insertTowns, connection))
+            {
+                command.Parameters.AddWithValue("@townName", townName);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private string GetVillainNames(int villainId, SqlConnection connection)
+        {
+            string nameSql = "SELECT Name FROM Villains WHERE Id = @id";
+
+            using (SqlCommand command = new SqlCommand(nameSql, connection))
+            {
+                command.Parameters.AddWithValue("@id", villainId);
+                return (string)command.ExecuteScalar();
             }
         }
 
@@ -150,30 +492,19 @@ namespace Fetching_Resultsets_ADO.NET
 
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
-                     if(!reader.HasRows)
+                    if (!reader.HasRows)
                     {
                         Console.WriteLine("(no minions)");
                     }
-                     else
+                    else
                     {
                         int counter = 0;
                         while (reader.Read())
-                        {                
+                        {
                             Console.WriteLine($"{++counter}. {reader[0]} {reader[1]} ");
                         }
                     }
                 }
-            }
-        }
-
-        private string GetVillianName(int villainId, SqlConnection connection)
-        {
-            string nameSql = "SELECT Name FROM Villains WHERE Id = @id";
-
-            using (SqlCommand command = new SqlCommand(nameSql, connection))
-            {
-                command.Parameters.AddWithValue("@id", villainId);
-                return (string)command.ExecuteScalar();
             }
         }
 
--- Databases Advanced - Entity Framework/L01.Databases Advanced_EntityFramework/L01.Databases Advanced_EntityFramework/StartUp.cs
+++ Databases Advanced - Entity Framework/L01.Databases Advanced_EntityFramework/L01.Databases Advanced_EntityFramework/StartUp.cs
@@ -11,10 +11,14 @@ namespace Fetching_Resultsets_ADO.NET
 
             Problems problems = new Problems();
 
-            // problems.InitialSetup(Configuration.ConnectionString); // P01.Initial Setup
-            // problems.VillainNames(Configuration.ConnectionString); // P02.Villain Names
-            problems.MinionNames(Configuration.ConnectionString);
-
+        // problems.InitialSetup(Configuration.ConnectionString);           //  P01.Initial Setup
+        // problems.VillainNames(Configuration.ConnectionString);           //  P02.Villain Names
+        // problems.MinionNames(Configuration.ConnectionString);            //  P03.Minion Names
+        // problems.AddMinion(Configuration.ConnectionString);              //  P04.Add Minion
+        // problems.ChangeTownNamesCasing(Configuration.ConnectionString);  //  P05.ChangeTownNamesCasing
+        // problems.PrintAllMinionsNames(Configuration.ConnectionString);   //  P07.PrintAllMinionsNames
+        // problems.IncreaseMinionAge(Configuration.ConnectionString);      //  P08.IncreaseMinionAge 
+        problems.IncreaseAgeStoredProcedure(Configuration.ConnectionString);// P09.IncreaseMinionAge
 
 
 
