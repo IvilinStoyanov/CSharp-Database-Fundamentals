8d528a237bb1839f02bdb00eb4568327a8a76e85
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-07-01 16:57:43 +0300

    Adding functionals to the DbSetContext class

--- Databases Advanced - Entity Framework/ORM Fundamentals/MiniORM/DbContext.cs
+++ Databases Advanced - Entity Framework/ORM Fundamentals/MiniORM/DbContext.cs
@@ -1,4 +1,48 @@
-﻿namespace MiniORM
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MiniORM
 {
-	// TODO: Create your DbContext class here.
+    public abstract class DbContext
+    {
+        private readonly DatabaseConnection connection;
+
+        private readonly Dictionary<Type, PropertyInfo> dbSetProperties;
+
+
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime)
+        };
+
+        protected DbContext(string connectionString)
+        {
+            this.connection = new DatabaseConnection(connectionString);
+
+            this.dbSetProperties = this.DiscoverDbSet();
+
+            using (new ConnectionManager(connection))
+            {
+                this.InitializeDbSets();
+            }
+
+            this.MapAllRelations();
+        }
+
+        public void SaveChanges()
+        {
+            var dbSet = this.dbSetProperties
+                .Select(pi => pi.Value.GetValue(this))
+                .ToArray();
+        }
+    }
 }
