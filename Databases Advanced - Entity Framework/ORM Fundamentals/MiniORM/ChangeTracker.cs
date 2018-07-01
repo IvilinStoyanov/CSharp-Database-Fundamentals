23b5f06fc965a12451ca4c963d9bf78c6d8baae3
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-07-01 10:10:20 +0300

    Adding the ORM(Part I)

--- Databases Advanced - Entity Framework/ORM Fundamentals/MiniORM/ChangeTracker.cs
+++ Databases Advanced - Entity Framework/ORM Fundamentals/MiniORM/ChangeTracker.cs
@@ -1,4 +1,34 @@
-﻿namespace MiniORM
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
 {
-	// TODO: Create your ChangeTracker class here.
+    // TODO: Create your ChangeTracker class here.
+    internal class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+            this.removed = new List<T>();
+
+            this.allEntities = CloneEntities(entities);
+        }
+
+        private static List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            var cloneEntities = new List<T>();
+
+            var propertiesToClone = typeof(T).GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType))
+                .ToArray();
+        }
+    }
 }
