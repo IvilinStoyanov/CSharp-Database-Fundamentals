418a33a3a027fd613408d9a7d39be7efa3c520de
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-07-02 14:34:17 +0300

    till Part III

--- Databases Advanced - Entity Framework/ORM Fundamentals/MiniORM/DbContext.cs
+++ Databases Advanced - Entity Framework/ORM Fundamentals/MiniORM/DbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 
@@ -40,9 +41,57 @@ namespace MiniORM
 
         public void SaveChanges()
         {
-            var dbSet = this.dbSetProperties
+            var dbSets = this.dbSetProperties
                 .Select(pi => pi.Value.GetValue(this))
                 .ToArray();
+
+            foreach (IEnumerable<object> dbSet in dbSets)
+            {
+                var invalidEntities = dbSet
+                    .Where(entity => !IsObjectValid(entity))
+                    .ToArray();
+
+                if (invalidEntities.Any())
+                {
+                    throw new InvalidOperationException(
+                        $"{invalidEntities.Lenght} Invalid Entities found in {dbSet.GetType().Name}");
+                }
+
+            }
+            using (new ConnectionManager(connection))
+            {
+                using (var transaction = this.connection.StartTransaction())
+                {
+                    foreach (IEnumerable<object> dbSet in dbSets)
+                    {
+                        var dbSetType = dbSet.GetType().GetGenericArgumets().First();
+
+                        var persistMethod = typeof(DbContext)
+                            .GetMethod("Persist", BindingFlags.Instance | BindingFlags.NonPublic)
+                            .MakeGenericMethod(dbSetType);
+
+                        try
+                        {
+                            persistMethod.Invoke(this, new object[] { dbSet });
+                        }
+                        catch(TargetInvocationException tie)
+                        {
+                            throw tie.InnerException;
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                        catch (SqlException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+                    transaction.Commit();
+                }
+            }
         }
     }
 }
