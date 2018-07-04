63674676dac59ae9745c92fbbf7524d19ef70cb5
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-07-04 19:50:15 +0300

    Adding till Problem 02

---
+++ Databases Advanced - Entity Framework/IntroductionToEntityFramework/IntroductionToEntityFramework/Data/Models/Town.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02_DatabaseFirst.Data.Models
+{
+    public class Town
+    {
+        public Town()
+        {
+            Address = new HashSet<Address>();
+        }
+
+        public int TownId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Address> Address { get; set; }
+    }
+}
