97f7d559b8bd052f5313e50aafba85f7e158d42b
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-07-09 18:04:03 +0300

    Adding Code-First Homework till Problem 02

---
+++ Databases Advanced - Entity Framework/Code-First/P01_HospitalDatabase.Data/Migrations/20180709143328_AddDoctor2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_HospitalDatabase.Data.Migrations
+{
+    public partial class AddDoctor2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialty",
+                table: "Doctors",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Specialty",
+                table: "Doctors",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
