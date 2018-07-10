095e3eb139b52bcb02bbc954bb927b34de78a254
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-07-10 18:13:32 +0300

    Adding SalesDb(Code-First)

---
+++ Databases Advanced - Entity Framework/Code-First/SalesDatabase/P03_SalesDatabase.Data/Models/20180710135007_ProductsAddColumnDescription.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public partial class ProductsAddColumnDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                maxLength: 250,
+                nullable: true,
+                defaultValue: "No description",
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldDefaultValue: "No description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "No description",
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldNullable: true,
+                oldDefaultValue: "No description");
+        }
+    }
+}
