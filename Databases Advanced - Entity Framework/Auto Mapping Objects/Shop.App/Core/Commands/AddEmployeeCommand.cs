6f1e59cbb96b3476aa92dee19d9a71096c9edc69
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-07-25 22:49:10 +0300

    Adding functionality to EmployeeDto

--- Databases Advanced - Entity Framework/Auto Mapping Objects/Shop.App/Core/Commands/AddEmployeeCommand.cs
+++ Databases Advanced - Entity Framework/Auto Mapping Objects/Shop.App/Core/Commands/AddEmployeeCommand.cs
@@ -1,14 +1,36 @@
-﻿using System;
+﻿using Shop.App.Core.Contracts;
+using Shop.App.Core.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Shop.App.Core.Commands
 {
-   public class AddEmployeeCommand
+   public class AddEmployeeCommand : ICommand
     {
-        public AddEmployeeCommand()
-        {
+        private readonly IEmployeeController employeeController;
 
+        public AddEmployeeCommand(IEmployeeController employeeController)
+        {
+            this.employeeController = employeeController;
+        }
+
+        public string Exucute(string[] args)
+        {
+            string firstName = args[0];
+            string lastName = args[1];
+            decimal salary = decimal.Parse(args[2]);
+
+            EmployeeDto employeeDto = new EmployeeDto
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Salary = salary
+            };
+
+            this.employeeController.AddEmployee(employeeDto);
+
+            return $"Employee {firstName} {lastName} added succesfully";
         }
     }
 }
