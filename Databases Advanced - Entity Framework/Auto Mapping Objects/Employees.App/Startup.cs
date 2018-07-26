84aaaaacdf0d7e5330ce1da2146da39b34a7db5b
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-07-26 14:31:35 +0300

    Adding Part III

---
+++ Databases Advanced - Entity Framework/Auto Mapping Objects/Employees.App/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace Employees.App
+{
+    using AutoMapper;
+    using Core;
+    using Employees.App.Interfaces;
+    using Employees.App.IO;
+    using Employees.Data;
+    using Employees.Data.Config;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            InitializeMapper();
+
+            var reader = new ConsoleReader();
+            var writer = new ConsoleWriter();
+            var serviceProvider = ConfigureServices();
+            var commandInterpreter = new CommandInterpreter<ICommand>(serviceProvider);
+
+            var engine = new Engine(reader, writer, commandInterpreter);
+            engine.Run();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddDbContext<EmployeesContext>(options =>
+                options.UseSqlServer(DbConfig.ConnectionString)
+            );
+
+            serviceCollection.AddTransient<IWriter, ConsoleWriter>();
+            serviceCollection.AddTransient<IReader, ConsoleReader>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            return serviceProvider;
+        }
+
+        private static void InitializeMapper()
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile<MapperProfile>());
+        }
+    }
+}
---
+++ Databases Advanced - Entity Framework/Auto Mapping Objects/Employees.Data/Config/DbConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Employees.Data.Config
+{
+    public class DbConfig
+    {
+        public const string ConnectionString =
+            @"Server=GIGABYTE-PC\SQLEXPRESS;Database=Shop;Integrated Security=True;";
+    }
+}
