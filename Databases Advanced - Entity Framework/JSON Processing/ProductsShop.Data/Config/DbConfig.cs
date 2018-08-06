adbe56345d655b65f729f32d25f7006047aad0b1
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-08-06 18:23:15 +0300

    Removing some files

--- Databases Advanced - Entity Framework/JSON Processing/ProductsShop.Data/Config/DbConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductsShop.Data.Config
-{
-    public class DbConfig
-    {
-        public const string ConnectionString
-            = @"Server=GIGABYTE-PC\SQLEXPRESS; Database=ProductShop;Integrated Security=True";
-    }
-}
