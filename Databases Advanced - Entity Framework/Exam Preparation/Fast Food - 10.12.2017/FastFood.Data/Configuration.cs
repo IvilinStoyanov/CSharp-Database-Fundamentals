f8b835a4c55ea143c4d7d218f502dc6db8a407b7
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-08-08 21:21:30 +0300

    Adding till Part 1(50/50)

---
+++ Databases Advanced - Entity Framework/Exam Preparation/Fast Food - 10.12.2017/FastFood.Data/Configuration.cs
@@ -0,0 +1,8 @@
+namespace FastFood.Data
+{
+	public static class Configuration
+	{
+		public static string ConnectionString = 
+            @"Server=GIGABYTE-PC\SQLEXPRESS;Database=FastFood;Trusted_Connection=True";
+	}
+}
