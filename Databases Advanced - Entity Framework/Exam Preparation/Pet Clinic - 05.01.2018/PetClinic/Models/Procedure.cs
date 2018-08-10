4808f1f558c97ba911e0c1418ce4b403b1e33d93
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-08-10 22:17:31 +0300

    Adding Model Definition(50/50)

---
+++ Databases Advanced - Entity Framework/Exam Preparation/Pet Clinic - 05.01.2018/PetClinic/Models/Procedure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace PetClinic.Models
+{
+   public class Procedure
+    {
+        public Procedure()
+        {
+            this.ProcedureAnimalAids = new List<ProcedureAnimalAid>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public int AnimalId { get; set; }
+        public Animal Animal { get; set; }
+
+        [Required]
+        public int VetId { get; set; }
+        public Vet Vet  { get; set; }
+
+        [NotMapped]
+        public decimal Cost => this.ProcedureAnimalAids.Sum(x => x.AnimalAid.Price);
+
+        public DateTime DateTime { get; set; }
+
+        public ICollection<ProcedureAnimalAid> ProcedureAnimalAids { get; set; }
+    }
+}
